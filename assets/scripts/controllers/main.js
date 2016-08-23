3c32ac80d05932ec64de34bf93d43af003406658
RoySegall <roy.segall@gizra.com>
2016-08-23 10:35:40 +0300

    Foo,.

---
+++ assets/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dc15App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dc15App
+ */
+angular.module('dc15App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
